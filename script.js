6eb4cef28c038a8a057d2a1f6a42d1bfd0654d00
jangmz <131482887+jangmz@users.noreply.github.com>
2023-09-08 11:50:13 +0200

    added new function generateGrid, ability to remove the current grid, later ability to create new grid of different size

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-// displays the grid
+function generateGrid(gridSize) {
+    const gridRows = document.querySelectorAll(".grid-row");
+    gridRows.forEach(gridRow => grid.removeChild(gridRow));
+    //console.log("grid removed");
+}
+
 const grid = document.querySelector(".grid");
 
+// displays the grid
 for (let col = 0; col < 16; col++) {
     const rowDiv = document.createElement("div");
     rowDiv.classList.add("grid-row");
@@ -18,3 +24,11 @@ const gridPixels = document.querySelectorAll(".grid-pixel");
 gridPixels.forEach(gridPixel => gridPixel.addEventListener("mouseover", (e) => {
     e.target.classList.add("bgColor");
 }));
+
+// input for changing the grid size
+const sizeBtn = document.querySelector("#size-btn");
+sizeBtn.addEventListener("click", () => {
+    const newGridSize = prompt("Enter new size (max 100)");
+    //console.log(newGridSize);
+    generateGrid(newGridSize);
+})
