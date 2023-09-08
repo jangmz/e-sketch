eea806ff7b40729fd077675f5462607aa9972a42
jangmz <131482887+jangmz@users.noreply.github.com>
2023-09-08 10:41:57 +0200

    display of basic grid, added event listener on mouseover to change bg color

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const container = document.querySelector(".container");
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("pixel");
-    container.appendChild(div);
+// displays the grid
+const grid = document.querySelector(".grid");
+
+for (let col = 0; col < 16; col++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.classList.add("grid-row");
+    for (let row = 0; row < 16; row++) {
+        const pixelDiv = document.createElement("div");
+        pixelDiv.classList.add("grid-pixel");
+        
+        rowDiv.appendChild(pixelDiv);
+    }
+    grid.appendChild(rowDiv);
 }
+
+// change the background color on hover event
+const gridPixels = document.querySelectorAll(".grid-pixel");
+gridPixels.forEach(gridPixel => gridPixel.addEventListener("mouseover", (e) => {
+    e.target.classList.add("bgColor");
+}));
